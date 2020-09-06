4518f361096df7a2a5f5a977fef63bffffc7bca0
swoods30311 <swoods30311@gmail.com>
2020-09-06 01:03:49 -0400

    Initial Commit

---
+++ src/Game.java
@@ -0,0 +1,64 @@
+public class Game {
+    public static final int MAX_MISSES = 7;
+    private String answer;
+    private String hits;
+    private String misses;
+
+//olyjoshone
+    private char normalizeGuess(char letter){
+        if(! Character.isLetter(letter)){
+            throw new IllegalArgumentException("a letter is required");
+        }
+        letter = Character.toLowerCase(letter);
+        if (misses.indexOf(letter) != -1 || hits.indexOf(letter) != -1 ){
+            throw new IllegalArgumentException(
+                    letter + " has already been guessed! Pick another letter.");
+        }
+        return letter;
+    }
+
+    public Game(String answer){
+       this.answer = answer.toLowerCase();
+       hits = "";
+       misses = "";
+    }
+
+    public String getAnswer(){
+        return answer;
+    }
+
+    public boolean applyGuess(String letters){
+        if(letters.length() == 0){
+            throw new IllegalArgumentException("No letter found.");
+        }
+        return applyGuess(letters.charAt(0));
+    }
+    public boolean applyGuess(char letter){
+        letter = normalizeGuess(letter);
+        boolean isHit = answer.indexOf(letter) != -1;
+        if(isHit){
+            hits += letter;
+        }else{
+            misses += letter;
+        }
+        return isHit;
+    }
+    public int getRemainingTries(){
+        return MAX_MISSES - misses.length();
+    }
+    public String getCurrentProgress(){
+        String progress = "";
+        for(char letter: answer.toCharArray()){
+            char display = '-';
+            if(hits.indexOf(letter) != -1){
+                display = letter;
+            }
+            progress += display;
+        }
+            return progress;
+    }
+    public boolean isWon(){
+        return getCurrentProgress().indexOf('-') == -1;
+    }
+
+}
